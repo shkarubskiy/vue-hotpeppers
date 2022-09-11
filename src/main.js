51bce7dec98b9be9641b429f397768b85063de27
shkarubskiy <vshkarubskiy@gmail.com>
2022-09-11 11:38:26 +0600

    feat: update layouts, add sauce photos

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import router from "./router";
 import store from "./store";
 import SocialBlock from "@/components/SocialBlock.vue";
 import PepperCard from "@/components/PepperCard.vue";
+import PeppersBlock from "@/components/PeppersBlock.vue";
 
 const app = createApp(App).use(store).use(router);
 app.component("SocialBlock", SocialBlock);
 app.component("PepperCard", PepperCard);
+app.component("PeppersBlock", PeppersBlock);
 app.mount("#app");
